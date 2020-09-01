6bb8ab801acbf02fd65d941f54a19adff5a3fc91
Allan Kerr <allan.kerr@D2L.com>
2020-09-01 13:05:39 -0400

    Add index.js to create shared server logger

---
+++ index.js
@@ -0,0 +1,12 @@
+import { LoggingClient, ServerLogger } from './logging.js';
+
+// Max batch size based on Kinesis batch limit
+const batchSize = 500;
+
+// Send a log batch if nothing has logged for 5 seconds
+const batchTime = 5000;
+
+const serverLogger = new ServerLogger(batchSize, batchTime);
+export function createClient(appId) {
+	return new LoggingClient(appId, serverLogger);
+}
